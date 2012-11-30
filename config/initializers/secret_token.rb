2b4ac7af9d1aea6b3b315013700ffce34d039483
prromiz <prromiz@gmail.com>
2012-11-30 12:14:44 +0200

    1 page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Justdoit::Application.config.secret_token = '5404129bdfd6e60bb6f4d4f55c4183d11f46006c7198ef16ebc2c3ae8ba4bd2b473152f4f6f61a5730901b2b997177b16ba6d57afbad2fbe3fb83c62d3d96630'
